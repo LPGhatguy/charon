7f0d2277ab01fa12c978e912e5af1e0a4b38f799
Lucien Greathouse <me@lpghatguy.com>
2020-03-21 23:19:51 -0700

    Implement FromUriComponent for more types.
    Closes #3.

--- src/traits.rs
+++ src/traits.rs
@@ -8,8 +8,35 @@ pub trait FromUriComponent: Sized {
     fn read(component: &str) -> Option<Self>;
 }
 
-impl FromUriComponent for u32 {
+impl FromUriComponent for String {
     fn read(component: &str) -> Option<Self> {
-        component.parse().ok()
+        Some(component.to_owned())
     }
+}
+
+macro_rules! from_uri_for_parseables {
+    ( $( $ty: ty, )+ ) => {
+        $(
+            impl FromUriComponent for $ty {
+                fn read(component: &str) -> Option<Self> {
+                    component.parse().ok()
+                }
+            }
+        )+
+    };
+}
+
+from_uri_for_parseables! {
+    u8,
+    u16,
+    u32,
+    u64,
+    u128,
+    i8,
+    i16,
+    i32,
+    i64,
+    i128,
+    f32,
+    f64,
 }
